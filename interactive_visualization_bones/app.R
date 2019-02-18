549e2994986c3529752aa4558a31ad5f4de0d962
Fernando Hoces de la Guardia <fhoces@users.noreply.github.com>
2019-02-17 23:23:24 -0800

    addede comments

--- interactive_visualization_bones/app.R
+++ interactive_visualization_bones/app.R
@@ -1,11 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Template for a shiny app fo OPA
+# based on the first oficial app for OPA created by Sara Stoudt 
+# (link here: )
 
 library(shiny)
 
