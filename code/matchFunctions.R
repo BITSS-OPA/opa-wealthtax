5fb8a9b99f513596ff0a284e71d049054e5f40d2
sastoudt <sstoudt@berkeley.edu>
2019-02-21 17:56:56 -0800

    try to match more, some discrepancies to figure out

--- code/matchFunctions.R
+++ code/matchFunctions.R
@@ -66,3 +66,98 @@ return(list("numberTaxpayers_mo" = numberTaxpayers_mo,
 
 }
 invisible( list2env(tax_revenue_mo_f(),.GlobalEnv) )
+
+#### total_rev_mo_f ####
+
+## don't need top_tax_base_var anymore?
+## so we don't need total_rev_mo_f()?
+
+# use this instead
+tax_revenue_mo_f()$tax_rev_init_mo
+
+#### ten_years_mo_f ####
+
+ten_years_mo_f <- function(inflation_var = inflation_so,
+                           population_gr_var = population_gr_so,
+                           real_growth_var = real_growth_so,
+                           total_rev_var = total_rev_pe, 
+                           top_tax_base_var = top_tax_base_in){
+  discount_rate_mo <- inflation_var + population_gr_so + real_growth_so  
+  ten_year_factor_mo <- sum( ( 1 + discount_rate_mo )^( 0:9 ) ) 
+  ## is this what turns into 13?
+  ## if so, I could adapt the Shiny app to fit better
+  
+  ten_year_revenue_pe <- round(total_rev_pe) * ten_year_factor_mo     
+  #ten_year_revenue_pe <- total_rev_pe * ten_year_factor_mo                      #PE
+  ten_year_top_tax_pe <- top_tax_base_var * ten_year_factor_mo          
+  #ten_year_top_tax_pe <- top_tax_base_var * ten_year_factor_mo                  #PE
+  return( list("discount_rate_mo" = discount_rate_mo, "ten_year_factor_mo" = ten_year_factor_mo, 
+               "ten_year_revenue_pe" = ten_year_revenue_pe, "ten_year_top_tax_pe" = ten_year_top_tax_pe) )
+}
+
+## I'll make your getGroup work in Shiny
+
+
+#### matching tax revenue function ####
+
+brackets_po <- c(0, 25, 50, 100, 250, 500, 1000) * 1e6
+tax_rates_po <- c(  0,    0, 0.02,  0.02,  0.02,  0.02, 0.03) 
+
+## Fernando's Tax Revenue 
+getTaxRevenue <- function(wealth_var = wealth_aux, taxrates_var = tax_rates_po,
+                          brackets_var = brackets_po) {
+ # browser()
+  ## expecting taxLevels in percentage
+  # taxLevels <- taxLevels / 100
+  if (length(brackets_var) != length(taxrates_var)){
+    stop("Tax brackets and tax rates do not match")
+  }
+  # Compute max taxable wealth per bracket
+  max_tax_per_brack <- c(diff(c(0, brackets_var)), 1e100)
+  # Substract wealth minus tax bracket. If wealth above a given bracket (difference is larger than max taxable wealth), 
+  # then assign max taxable wealth to that given bracket
+  to_tax <- ifelse( wealth_var - c(0, brackets_var) > max_tax_per_brack, 
+                    max_tax_per_brack, 
+                    ( wealth_var - c(0,brackets_var) ) )   
+  # If wealth if lower than a given bracket (difference between wealth and bracket is negative), then assign zero to that bracket  
+  to_tax <- ifelse( to_tax<0, 0, to_tax )
+  # Apply trax rates to each corresponding bracket and all together
+  total_tax <- sum( to_tax * c(0, taxrates_var) )   
+  return(total_tax)
+}
+
+## Sara's Tax Revenue 
+getAverageTax <- function(wealth, taxLevels, brackets) {
+  ## pass in brackets to make sure they update
+  ## expecting taxLevels in percentage
+  taxLevels <- taxLevels / 100
+  first <- wealth - brackets[1] * 1e6
+  second <- first - (brackets[2] * 1e6 - brackets[1] * 1e6)
+  third <- second - (brackets[3] * 1e6 - brackets[2] * 1e6)
+  fourth <- third - (brackets[4] * 1e6 - brackets[3] * 1e6)
+  
+  firstChunk <- ifelse(second >= 0, taxLevels[1] * (brackets[2] * 1e6 - brackets[1] * 1e6), taxLevels[1] * max(first, 0))
+  secondChunk <- ifelse(third >= 0, taxLevels[2] * (brackets[3] * 1e6 - brackets[2] * 1e6), taxLevels[2] * max(second, 0))
+  thirdChunk <- ifelse(fourth >= 0, taxLevels[3] * (brackets[4] * 1e6 - brackets[3] * 1e6), taxLevels[3] * max(third, 0))
+  fourthChunk <- ifelse(fourth >= 0, fourth * taxLevels[4], 0)
+  
+  toReturn <- firstChunk + secondChunk + thirdChunk + fourthChunk
+  return(toReturn)
+}
+
+
+getTaxRevenue(1e8,tax_rates_po,brackets_po)
+getAverageTax(1e8,tax_rates_po*100,brackets_po/1e6)
+
+lapply(c(1e6,25e6,75e6,1100e6),getAverageTax,tax_rates_po*100,brackets_po/1e6)
+lapply(c(1e6,25e6,75e6,1100e6),getTaxRevenue,tax_rates_po,brackets_po)
+
+## 1100e6 doesn't match
+
+lapply(c(0, 25, 50, 100, 250, 500, 1000) * 1e6,getAverageTax,tax_rates_po*100,brackets_po/1e6)
+lapply(c(0, 25, 50, 100, 250, 500, 1000) * 1e6,getTaxRevenue,tax_rates_po,brackets_po)
+## these all match
+
+lapply((10+c(0, 25, 50, 100, 250, 500, 1000)) * 1e6,getAverageTax,tax_rates_po*100,brackets_po/1e6)
+lapply((10+c(0, 25, 50, 100, 250, 500, 1000)) * 1e6,getTaxRevenue,tax_rates_po,brackets_po)
+## last one doesn't match
