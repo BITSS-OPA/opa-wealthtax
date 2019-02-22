c482a2e5beb9bc842c305d435dfdf2ecfce3449c
sastoudt <sstoudt@berkeley.edu>
2019-02-21 16:21:27 -0800

    make helper function example

--- code/matchFunctions.R
+++ code/matchFunctions.R
@@ -1,7 +1,34 @@
+#### my streamlined helper function version ####
+
+# this is more like what I could call in Shiny
+tax_revenue_helper <- function(numberTaxpayers_mo = numberTaxpayers_mo,
+                               tax_base_mo = tax_base_mo,
+                               brackets_var = brackets_po,
+                               main_tax_var = main_tax_po){
+  
+  target_hhlds_mo <- sum(numberTaxpayers_mo[which(main_tax_po>0)])
+  
+  tax_base_total_mo <- sum(tax_base_mo[which(main_tax_po>0)])/1000
+  tax_rev_init_mo <-   tax_base_total_mo * main_tax_var * 1000
+  return(list( "target_hhlds_mo" = target_hhlds_mo,
+               "tax_base_total_mo" = tax_base_total_mo,
+               "tax_rev_init_mo" = tax_rev_init_mo))
+}
+
 #### tax_revenue_mo_f #### 
 
-## start with cum_numberTaxpayers
-## calculate what you want for that outside of this function
+tax_revenue_mo_f <- function(
+  cum_numberTaxpayers_dina_var = cum_numberTaxpayers_dina_in,
+  cum_numberTaxpayers_scf_var = cum_numberTaxpayers_scf_in,
+  num_billionares_var = num_billionares_in,
+  cum_tax_base_dina_var = cum_tax_base_dina_in,
+  cum_tax_base_scf_var = cum_tax_base_scf_in,
+  brackets_var = brackets_po,
+  starting_brack_var = starting_brack_po,
+  main_tax_var = main_tax_po){
+  
+  ## start with cum_numberTaxpayers
+  ## calculate what you want for that outside of this function  
 
 cum_numberTaxpayers = (cum_numberTaxpayers_dina_var + cum_numberTaxpayers_scf_var)/2
 
@@ -23,24 +50,19 @@ tax_base_mo <- c(-1 * diff(cum_tax_base, lag = 1), top_tax_base_in) * 100
 ## don't need to return numberTaxpayers_mo and tax_base_mo since we input them
 ## we don't need starting_brack_var, we can recover it using brackets_var and main_tax_var
 
-## my streamlined version
-tax_revenue_mo_f <- function(numberTaxpayers_mo = numberTaxpayers_mo,
-                             tax_base_mo = tax_base_mo,
-                             brackets_var = brackets_po,
-                             main_tax_var = main_tax_po){
- 
-  target_hhlds_mo <- sum(numberTaxpayers_mo[which(main_tax_po>0)])
+helperResults = tax_revenue_helper(numberTaxpayers_mo = numberTaxpayers_mo,
+                   tax_base_mo = tax_base_mo,
+                   brackets_var = brackets_po,
+                   main_tax_var = main_tax_po)
 
-  tax_base_total_mo <- sum(tax_base_mo[which(main_tax_po>0)])/1000
-  tax_rev_init_mo <-   tax_base_total_mo * main_tax_var * 1000
-  return(list( "target_hhlds_mo" = target_hhlds_mo,
-               "tax_base_total_mo" = tax_base_total_mo,
-               "tax_rev_init_mo" = tax_rev_init_mo))
+
+return(list("numberTaxpayers_mo" = numberTaxpayers_mo,
+            "tax_base_mo" = tax_base_mo,
+            "tax_base_dina_mo" = tax_base_dina_mo,
+            "tax_base_scf_mo" = tax_base_scf_mo,
+            "target_hhlds_mo" = helperResults$target_hhlds_mo,
+            "tax_base_total_mo" = helperResults$tax_base_total_mo,
+            "tax_rev_init_mo" = helperResults$tax_rev_init_mo))
+
 }
 invisible( list2env(tax_revenue_mo_f(),.GlobalEnv) )
-
-## now more analogous to my
-householdsTaxed
-## do we ever use/need total tax base on its own?
-totalTax
-
