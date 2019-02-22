c84f0c0ef13dc8ffe82cc1242a548093b9ee9607
ktodomo <kdonnellymoran@gmail.com>
2019-02-22 15:52:49 -0800

    edits to dynamic document for clarity

--- interactive_visualization/runApp.R
+++ interactive_visualization/runApp.R
@@ -1,5 +1,6 @@
 library(shiny)
-wd = "/Users/fhoces/Desktop/sandbox/opa-wealthtax" ## put path to opa-wealthtax directory here
+#wd = "/Users/fhoces/Desktop/sandbox/opa-wealthtax" ## put path to opa-wealthtax directory here
+wd = "C:/Users/clanc/o-lab/opa-wealthtax"
 setwd(wd)
 
 runApp("interactive_visualization")
