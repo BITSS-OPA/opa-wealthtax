6251b12c5da5e9f65dc8b01ea39b1dcd65256a03
sastoudt <sstoudt@berkeley.edu>
2019-02-22 18:05:35 -0800

    force ui to match kateSuggestion branch

--- interactive_visualization/ui.R
+++ interactive_visualization/ui.R
@@ -5,34 +5,34 @@ library(ggplot2)
 ## template from here
 ## https://github.com/rstudio/shiny-examples/tree/master/051-movie-explorer
 ui <-
-
+  
   fluidPage(
     titlePanel("Wealth Tax Explorer"),
-
-
-
+    
+    
+    
     fluidRow(
       column(
         2,
         wellPanel(
           sliderInput("bracket1", "Apply a tax of:",
-            0, 10, 1,
-            step = 1, post = " %" # , ticks = F
+                      0, 10, 0,
+                      step = 1, post = " %" # , ticks = F
           ),
           sliderInput("bracket2", "Apply a tax of:",
-            0, 10, 1,
-            step = 1, post = " %" # , ticks = F
+                      0, 10, 2,
+                      step = 1, post = " %" # , ticks = F
           ),
           sliderInput("bracket3", "Apply a tax of:",
-            0, 10, 1,
-            step = 1, post = " %" # , ticks = F
+                      0, 10, 2,
+                      step = 1, post = " %" # , ticks = F
           ),
           sliderInput("bracket4", "Apply a tax of:",
-            0, 10, 2,
-            step = 1, post = " %" # , ticks = F
+                      0, 10, 3,
+                      step = 1, post = " %" # , ticks = F
           )
         ),
-
+        
         h6("Policy Analysis by:"),
         tags$a(href = "https://eml.berkeley.edu/~saez/", "Emmanuel Saez"),
         h6("and"),
@@ -48,41 +48,48 @@ ui <-
         2,
         wellPanel(
           sliderInput("bracketV1",
-            label = "to wealth above: ", min = 0,
-            max = 1000, step = 5, value = 10, post = " (m)"
+                      label = "to wealth above: ", min = 0,
+                      max = 1000, step = 5, value = 10, post = " (m)"
           ),
           sliderInput("bracketV2",
-            label = "to wealth above: ", min = 0,
-            max = 1000, step = 5, value = 50, post = " (m)"
+                      label = "to wealth above: ", min = 0,
+                      max = 1000, step = 5, value = 50, post = " (m)"
           ),
           sliderInput("bracketV3",
-            label = "to wealth above:", min = 500,
-            max = 1500, step = 5, value = 500, post = " (m)"
+                      label = "to wealth above:", min = 500,
+                      max = 1500, step = 5, value = 100, post = " (m)"
           ),
           sliderInput("bracketV4",
-            label = "to wealth above:", min = 1000,
-            max = 10000, step = 100, value = 1000, post = " (m)"
+                      label = "to wealth above:", min = 1000,
+                      max = 10000, step = 100, value = 1000, post = " (m)"
           )
         ),
         h6("Assisted by:"),
         h6(""),
         h6("Katie Donnelly Moran"),
-        #h6("get undergrad names"),
+        h6("and Clancy Green,"),
+        
+        h6("Deployment help by:"),
+        h6("Akcan Balkir,"),
+        h6("Lindsey Heagy"),
+        h6("Chris Holdgraf, and"),
+        h6("Yuvi Panda"),
+        
         tags$a(href = "https://www.splitwise.com/taxes/#/brackets/0|160|353|432|479|543/10.1|14.9|25.0|28.1|33#.0|35.1/params/1|1|1|0|1|15", "Inspired by this visualization")
       ),
-
+      
       column(
         8,
         ggvisOutput("plot2"),
         # ggvisOutput("plotB"),
-
+        
         h3("Total Taxes ($bn)"),
         textOutput("totalTax"),
         h4("Total Taxes over 10 years ($t)"),
         textOutput("totalTax_10"),
-        h4("Total Taxpayers"), #(CHECK UNITS)
+        h4("Total Taxpayers"), ##  (CHECK UNITS)
         textOutput("totalTaxpayers"),
-        h4("Percentage of Affected"), # (CHECK UNITS)
+        h4("Percentage of Affected"), ## (CHECK UNITS)
         textOutput("percentHouseAffected")
       )
     )
